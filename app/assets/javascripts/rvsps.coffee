c240b7331ebdec86c81c6a7c43321716c8d4a117
Víctor <goldanel@gmail.com>
2019-05-21 12:37:11 +0100

    rsvp days are Wed and Thu; pax can't be negative

--- app/assets/javascripts/rvsps.coffee
+++ app/assets/javascripts/rvsps.coffee
@@ -3,6 +3,6 @@ $ ->
     format: "dd/mm/yyyy"
     weekStart: 1
     startDate: 'today'
-    daysOfWeekDisabled: '0,1,4,5,6'
-    daysOfWeekHighlighted: '2,3'
+    daysOfWeekDisabled: '0,1,2,5,6'
+    daysOfWeekHighlighted: '3,4'
     todayHighlight: true
--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,6 +1,6 @@
 class Rsvp < ApplicationRecord
   validate :rsvps_can_only_be_wed_thu, on: [:create, :update]
-  validates :pax, numericality: { only_integer: true }
+  validates :pax, numericality: { only_integer: true, greater_than: 0 }
   validates :name, presence: true
   validates :telephone, presence: true, length: {in: 9..15}
 
@@ -11,8 +11,8 @@ class Rsvp < ApplicationRecord
 private
 
   def rsvps_can_only_be_wed_thu #rsvps only admited for Tuesdays and Wednesdays
-    if !(self.rsvp_date.strftime("%a") == "Tue" || self.rsvp_date.strftime("%a") == "Wed")
-      self.errors.add(:rsvp_date, "Sólo se admiten reservas Martes y Miércoles")
+    if !(self.rsvp_date.strftime("%a") == "Thu" || self.rsvp_date.strftime("%a") == "Wed")
+      self.errors.add(:rsvp_date, "Sólo se admiten reservas Miércoles y Jueves")
     end
   end
 
